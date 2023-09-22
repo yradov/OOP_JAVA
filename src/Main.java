5a2df8f68da20c2714bbd8f70215102cf6fe77fb
RYuriy <radov.yuriy@ukr.net>
2023-09-22 11:29:35 +0300

    + class Animal

--- src/Main.java
+++ src/Main.java
@@ -2,12 +2,15 @@ public class Main {
     public static void main(String[] args) {
         //System.out.printf("Hello and welcome!");
         int a = 1;
-        Animal animal1 = new Animal(10, "Animal");
+        Animal animal1 = new Animal(10, "Animal1");
+        Animal animal2 = new Animal(5, "Animal2");
 //        Animal animal1 = new Animal();
 //        animal1.age = 2;
 //        animal1.name = "Animal2";
         System.out.println("age: " + animal1.age);
         System.out.println("name: " + animal1.name);
+        animal1.makeSounds();
+        animal2.makeSounds();
     }
 }
 
@@ -21,4 +24,9 @@ class Animal {
         this.age = age;
         this.name = name;
     }
+
+    void makeSounds() {
+        System.out.println("animal " + this.name + " make sounds");
+    }
 }
+
