2076cb78b8c564479e424c3e26015121e7f6df34
RYuriy <radov.yuriy@ukr.net>
2023-09-22 11:19:15 +0300

    + class Animal

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,24 @@
-// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
-// then press Enter. You can now see whitespace characters in your code.
 public class Main {
     public static void main(String[] args) {
-        // Press Alt+Enter with your caret at the highlighted text to see how
-        // IntelliJ IDEA suggests fixing it.
-        System.out.printf("Hello and welcome!");
+        //System.out.printf("Hello and welcome!");
+        int a = 1;
+        Animal animal1 = new Animal(10, "Animal");
+//        Animal animal1 = new Animal();
+//        animal1.age = 2;
+//        animal1.name = "Animal2";
+        System.out.println("age: " + animal1.age);
+        System.out.println("name: " + animal1.name);
+    }
+}
 
-        // Press Shift+F10 or click the green arrow button in the gutter to run the code.
-        for (int i = 1; i <= 5; i++) {
+class Animal {
+    int age;
+    String name;
+    public Animal() {
+    }
 
-            // Press Shift+F9 to start debugging your code. We have set one breakpoint
-            // for you, but you can always add more by pressing Ctrl+F8.
-            System.out.println("i = " + i);
-        }
+    public Animal(int age, String name) {
+        this.age = age;
+        this.name = name;
     }
 }
