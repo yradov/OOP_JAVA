1ff0cb90a2d5908296a711f86d989aa9ea67283f
RYuriy <radov.yuriy@ukr.net>
2023-09-23 11:17:39 +0300

    + make class Animal as abstract

--- src/Main.java
+++ src/Main.java
@@ -2,15 +2,19 @@ public class Main {
     public static void main(String[] args) {
         //System.out.printf("Hello and welcome!");
         int a = 1;
-        Animal animal1 = new Animal(10, "Animal1");
-        Animal animal2 = new Animal(5, "Animal2");
+        /**
+         * 'Animal' is abstract; cannot be instantiated
+         * */
+//        Animal animal1 = new Animal(10, "Animal1");
+//        Animal animal2 = new Animal(5, "Animal2");
 //        Animal animal1 = new Animal();
 //        animal1.age = 2;
 //        animal1.name = "Animal2";
-        System.out.println("age: " + animal1.getAge());
-        System.out.println("name: " + animal1.getName());
-        animal1.makeSounds();
-        animal2.makeSounds();
+//        System.out.println("age: " + animal1.getAge());
+//        System.out.println("name: " + animal1.getName());
+//        animal1.makeSounds();
+//        animal2.makeSounds();
+
         System.out.println("===========================");
         Dog dog1 = new Dog(3, "Richi");
         System.out.println(dog1.getName());
@@ -33,7 +37,7 @@ public class Main {
     }
 }
 
-class Animal {
+abstract class Animal {
     private int age;
     private String name;
 
